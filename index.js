643356b03e6f65a485800d4ba860de6c0cefd185
olusola olagoroye <solaolagoroye@gmail.com>
2019-11-01 01:27:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ function prependKitten(name){
  return ["Arnold", ...kittens];
 }
 function removeLastKitten(){
-  return kittens.slice(0' kittens.length -1);
+  return kittens.slice(0, kittens.length -1);
 }
 function removeFirstKitten(){
   return kittens.slice(1);
